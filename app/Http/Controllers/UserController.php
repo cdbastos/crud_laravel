65b72459f5500442040b40e14d1ed417cc54ff56
Cristian Bastos <cristianbastos14@gmail.com>
2020-09-12 01:14:02 -0500

    rutas y controlador para usuario creado

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+    public function index()
+    {
+        # code...
+    }
+
+    public function store()
+    {
+        # code...
+    }
+
+    public function delete()
+    {
+        # code...
+    }
+}
